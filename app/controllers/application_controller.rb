7bc6d495634ee10d871da22c468d3c704e328006
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-11 23:27:05 -0700

    CRUD operation is working Correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "Hello, World"
+    def index
+        @users=User.all
+    end
+
+    def show
+
+    end
+
+    def new
+        
+    end
+
+    def edit
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+    end
 end
